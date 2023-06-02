0a10bb3d927192e907a5b13020ffde2e3cbd7c30
Faisal ahmed <faisal2410@yahoo.com>
2023-06-02 23:06:11 +0600

    initial commit

---
+++ database/migrations/2023_06_02_160503_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+             $table->id()->from( 1000 );
+            $table->string( 'title' )
+            ->unique();
+            $table->string( 'slug' )
+                ->unique();
+            $table->longText( 'description' );
+            $table->text( 'excerpt' )
+                ->comment( 'Summary of Post' );
+            $table->boolean( 'is_published' )
+                ->default( false );
+            $table->integer( 'min_to_read' )
+                ->nullable();
+            $table->timestamps();
+          
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
