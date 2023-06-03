d425d3791c9326d03f84221268b018971640ed7f
Faisal ahmed <faisal2410@yahoo.com>
2023-06-03 23:01:47 +0600

    updated

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
+
+class PostSeeder extends Seeder {
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void{
+$json = File::get( 'database/json/posts.json' );
+$posts = collect( json_decode( $json ) );
+
+
+$posts->each( function ( $post ) {
+    Post::create( [
+        "title"        => $post->title,
+        "slug"         => $post->slug,
+        "excerpt"      => $post->excerpt,
+        "description"  => $post->description,
+        "is_published" => $post->is_published,
+        "min_to_read"  => $post->min_to_read,
+    ] );
+} );
+
+
+
+    }
+}
